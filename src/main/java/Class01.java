0a9966e74fe80db15050853df43a475b2eb50cda
Turan <ttg4426@gmail.com>
2023-05-06 21:53:25 +0300

    V2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,7 +2,8 @@ public class Class01 {
     public static void main(String[] args) {
         //v1 çalışmalarım
         System.out.println("version 1 im");
-
+        // v2 çalışmalarım
+        System.out.println("versiyon 2");
 
 
 
